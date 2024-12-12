0f41cb054627839b9ffa469f0026dd825856f394
arcrad <jonathanmaiorana@gmail.com>
2024-12-11 23:36:38 -0500

    add basic sqlite test and commit test db file

--- index.js
+++ index.js
@@ -14,9 +14,32 @@ if(!process.env.DATABASE_URL) {
 console.log(`$DATABASE_URL = ${process.env.DATABASE_URL}`);
 const db = new sqlite3.Database(process.env.DATABASE_URL);
 
-app.get('/', (req, res) => {
-  res.redirect('/index.html');
-})
+db.run(`CREATE TABLE IF NOT EXISTS visits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_time INTEGER
+)`);
+
+app.get('/', async (req, res) => {
+  db.run('INSERT INTO visits (created_time) VALUES (unixepoch())');
+  function fetchVisits() {
+      return new Promise((resolve, reject) => {
+        db.get(
+          'SELECT count(*) as total from visits',
+          {},
+          (err, row) => {
+            if(err) {
+              return reject(err.message);
+            }
+            console.dir(row.total);
+            return resolve(row.total);
+          }
+        );
+    });
+  }
+  const total_visits = await fetchVisits(); 
+  console.log(`total_visits=${total_visits}`);
+  res.send(`Welcome! You are visitor ${total_visits}`);
+});
 
 app.use(express.static('public'));
 
