d1298f0cf7987899234717d82a7dd274b54b4026
arcrad <jonathanmaiorana@gmail.com>
2024-12-11 22:51:28 -0500

    test adding sqlite

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import fs from 'fs';
+import sqlite3 from 'sqlite3';
 
 const app = express();
 const port = 3000;
+
+//const db = new sqlite3.Database(process.env.DATABASE_URL);
+
+console.log(`process.env('DATABASE_URL')=${process.env.DATABASE_URL}`);
 
 app.get('/', (req, res) => {
   res.redirect('/index.html');
